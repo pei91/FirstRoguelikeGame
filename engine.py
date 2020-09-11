cc7cc460a339577a0461d7794cd5aebbfe9cdb64
Pei <peichuan@buffalo.edu>
2020-09-11 17:48:48 -0400

    First

---
+++ engine.py
@@ -0,0 +1,46 @@
+import tcod as libtcod
+from input_handlers import handle_keys
+
+def main():
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    libtcod.console_set_custom_font('./images/arial10X10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.console_init_root(screen_width,screen_height, 'libtcod tutorial revised', False)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        libtcod.console_set_default_foreground(0,libtcod.white)
+        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_flush()
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x +=dx
+            player_y +=dy
+
+        if exit:
+            return True
+        
+        if fullscreen: 
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen)
+
+        if key.vk == libtcod.KEY_ESCAPE:
+            return True
+
+
+if __name__ == '__main__':
+    main()
---
+++ input_handlers.py
@@ -0,0 +1,22 @@
+import tcod as libtcod
+
+def handle_keys(key):
+    # Movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0,-1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0,1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1,0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1,0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt + Enter: toggle Full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit
+        return {'exit': True}
+
+    # no key pressed
+    return {}
